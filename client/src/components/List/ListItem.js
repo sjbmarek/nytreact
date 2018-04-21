663e833d8b87fd35dd36e75e701f0b24be3e0bdf
Susan Marek <smarek@chaskamn.com>
2018-04-20 21:14:55 -0500

    button stuff

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -4,10 +4,10 @@ export const ListItem = props => (
   <li className="list-group-item">
     <a href = {props.url} target="_blank">{props.title}</a>
 
-    <button className="mr-1 btn-danger ml-1 delete-btn btn" {...props}>
+    <button className="mr-1 btn-danger ml-1 delete-btn btn">
     Delete
     </button>
-    <button className="mr-1 btn-info ml-1 save-btn btn" {...props}>
+    <button className="mr-1 btn-info ml-1 save-btn btn">
     Save
     </button>
 
